b8fef7ddf4ddf432cdca92eafabc45b5b487ad61
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-01 10:49:04 -0400

    have input array populating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: '',
+                  userArray: []
+                 
+                 };
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  
+  
+  handleChange(event) {
+
+    
+    this.setState({ value: event.target.value
+                    
+                    })
+  }
+
+   handleSubmit(event) {
+    
+    event.preventDefault()
+    const previousInput = this.state.value;
+    this.setState({ 
+      value: '',
+      userArray: [...this.state.userArray, previousInput]
+      
+      })
+
+       
+
+  }
+
+
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input type="submit" value="Submit" />
+        <p>{this.state.userArray}</p>
+      </form>
+    )
+  }
 }
 
 export default App;
+
